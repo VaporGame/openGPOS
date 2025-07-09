48e1693b171b162a556f74ba6044eec3fed8bf3c
VaporGame <gluch909@gmail.com>
2025-07-09 18:34:00 +0200

    optimize memcpy

--- libc/src/string/memcpy.c
+++ libc/src/string/memcpy.c
@@ -1,11 +1,30 @@
 #include <libc/string.h>
+#include <stdint.h>
 
 void *memcpy(void *destination, const void *source, size_t num) {
-    char *d = (char *)destination;
-    const char *s = (const char *)source;
+    uint32_t *d = (uint32_t *)destination;
+    //const char *s = (const char *)source;
+    const uint32_t *s = (const uint32_t *)source;
 
-    for (size_t i = 0; i < num ; i++) {
+    while (num > 0 && ((uintptr_t)d % 4 != 0 || (uintptr_t)s % 4 != 0)) {
+        *((char *)d) = *((const char *)s);
+        d = (uint32_t *)((char *)d + 1);
+        s = (const uint32_t *)((const char *)s + 1);
+        num--;
+    }
+
+    // Copy 32-bit words
+    size_t num_words = num / 4;
+    for (size_t i = 0; i < num_words; i++) {
         d[i] = s[i];
+    }
+
+    // Handle trailing unaligned bytes
+    size_t remaining_bytes = num % 4;
+    char *d_byte = (char *)d + num_words * 4;
+    const char *s_byte = (const char *)s + num_words * 4;
+    for (size_t i = 0; i < remaining_bytes; i++) {
+        d_byte[i] = s_byte[i];
     }
 
     return destination;
