5ae3f574473970b15b61efb342ec29acda4ddf31
VaporGame <gluch909@gmail.com>
2025-07-12 23:30:45 +0200

    update header file

--- libc/include/libc/string.h
+++ libc/include/libc/string.h
@@ -10,4 +10,8 @@ size_t strlen(const char *str);
 char *strncpy(char *dest, const char *src, size_t count);
 char *strncat(char * destination, char *source, size_t num);
 
+char *strchr(const char *str, int c);
+int strcmp(const char *str1, const char *str2);
+char* strtok(char* restrict str, const char* restrict delim);
+
 #endif //STRING_H
